10434106cc0d951037038e53786549195de1815c
SzDabrowski <szdabrowsky@gmail.com>
2023-09-05 16:24:47 +0200

    feat:added connection to mongo db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 4000;
+
+mongoose.connect('mongodb://localhost/flashcards')
+const db = mongoose.connection;
+db.on('error',(error) => console.log(error));
+db.once('open', () => console.log('Connected to database'))
+
 
 app.listen(PORT, () => {
   console.log(`Your app listening on port ${PORT}`);
