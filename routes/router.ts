b74c91fdb3faa5f9ea8beea85fb6415bd31f8be2
SzDabrowski <szdabrowsky@gmail.com>
2023-09-09 21:32:59 +0200

    endpoint fix

--- routes/router.ts
+++ routes/router.ts
@@ -5,30 +5,30 @@ const router = Router();
 
 router.get("/",async (req, res) => {
     try{
-        const cards = await Card.find();
+        const cards = await Card.find().sort({ createdAt: -1 });;
         res.status(200).send(cards);
     } catch (err){
-        res.status(500).json({message: err})
+        res.status(500).send('An error occurred while fetching the cards.');
     }
 })
 
 router.get("/author/:author",async (req, res) => {
     const author = req.params.author;
     try{
-        const cards = await Card.find({author});
+        const cards = await Card.find({author}).sort({ createdAt: -1 });;
         res.status(200).send(cards);
     } catch (err){
-        res.status(500).json({message: err})
+        res.status(500).send('An error occurred while fetching the cards for specified author.');
     }
 })
 
 router.get("/tags/:tag",async (req, res) => {
     const tag = req.params.tag;
     try{
-        const cards = await Card.find({tags: tag});
+        const cards = await Card.find({tags: tag}).sort({ createdAt: -1 });;
         res.status(200).send(cards);
     } catch (err){
-        res.status(500).json({message: err})
+        res.status(500).send('An error occurred while fetching the cards for specified.');
     }
 })
 
