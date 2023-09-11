19b525f4e93354b03f02d7fd8579d9d3774633d4
SzDabrowski <szdabrowsky@gmail.com>
2023-09-11 09:48:52 +0200

    feat:endpoit for editing cards

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+
 import "dotenv/config";
+import cardRoutes from "../routes/router"
 
 const app: Express = express();
 const PORT = process.env.PORT || 4000;
 
 if (process.env.MONGODB_URL) {
-  mongoose.connect(process.env.MONGODB_URL);
+  mongoose.connect(process.env.MONGODB_URL,{dbName: "Flashcards"});
 } else {
   console.log("Please pass the URL to database");
 }
@@ -14,6 +16,10 @@ if (process.env.MONGODB_URL) {
 const db = mongoose.connection;
 db.on("error", (error: string) => console.log(error));
 db.once("open", () => console.log("Connected to database"));
+
+app.use(express.json());
+
+app.use('/cards', cardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Your app listening at http://localhost:${PORT}`);
