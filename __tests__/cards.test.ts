efeb515420910d3b254ffdc1dffbf66d6a29e97b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-09-14 11:25:16 +0200

    feat: create tests for finding cards

--- __tests__/cards.test.ts
+++ __tests__/cards.test.ts
@@ -1,12 +1,36 @@
-﻿import supertest from "supertest";
-import mongoose from "mongoose";
+﻿import request from "supertest";
+import mongoose, { Document } from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { app } from "../utils/server";
+import { Card, ICard } from "../models/card";
 
 describe("cards", () => {
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
     await mongoose.connect(mongoServer.getUri());
+
+    const testCard1: Document<ICard> = new Card({
+      front: "Front 1",
+      back: "Back 1",
+      author: "Author1",
+      tags: ["tag1", "tag2"],
+    });
+    const testCard2: Document<ICard> = new Card({
+      front: "Front 2",
+      back: "Back 2",
+      author: "Author2",
+      tags: ["tag2", "tag3"],
+    });
+    const testCard3: Document<ICard> = new Card({
+      front: "Front 3",
+      back: "Back 3",
+      author: "Author1",
+      tags: ["tag3", "tag4"],
+    });
+
+    await testCard1.save();
+    await testCard2.save();
+    await testCard3.save();
   });
 
   afterAll(async () => {
@@ -18,10 +42,60 @@ describe("cards", () => {
     it("should return 404 when given card does not exist", async () => {
       const cardId = "asd-123";
 
-      await supertest(app)
+      await request(app)
         .get(`/cards/${cardId}`)
         .set("Authorization", "pss-this-is-my-secret")
         .expect(404);
+    });
+
+    it("should return 200", async () => {
+      const resp = await request(app)
+        .get("/cards")
+        .set("Authorization", "pss-this-is-my-secret");
+      expect(resp.status).toBe(200);
+    });
+
+    it("should return 3 cards", async () => {
+      const resp = await request(app)
+        .get("/cards")
+        .set("Authorization", "pss-this-is-my-secret");
+
+      expect(resp.body.length).toBe(3);
+    });
+
+    it("should return cards in the correct order ", async () => {
+      const resp = await request(app)
+        .get("/cards")
+        .set("Authorization", "pss-this-is-my-secret");
+
+      expect(resp.body[0].front).toBe("Front 3");
+      expect(resp.body[1].front).toBe("Front 2");
+      expect(resp.body[2].front).toBe("Front 1");
+    });
+
+    it("should return cards written by the requested author in the correct order ", async () => {
+      const resp = await request(app)
+        .get("/cards/author/Author1")
+        .set("Authorization", "pss-this-is-my-secret");
+
+      expect(resp.body[0].front).toBe("Front 3");
+      expect(resp.body[1].front).toBe("Front 1");
+    });
+
+    it("should return 2 cards written by the requested author ", async () => {
+      const resp = await request(app)
+        .get("/cards/author/Author1")
+        .set("Authorization", "pss-this-is-my-secret");
+
+      expect(resp.body.length).toBe(2);
+    });
+
+    it("should return 2 cards with the requested tag ", async () => {
+      const resp = await request(app)
+        .get("/cards/tags/tag2")
+        .set("Authorization", "pss-this-is-my-secret");
+
+      expect(resp.body.length).toBe(2);
     });
   });
 });
